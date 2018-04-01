9dc1344d5ff64f91d8bcab50e9ce9e8dc7ddcbdd
Ryan Burns <rburns@caltech.edu>
2018-04-01 15:27:16 -0700

    Store screen position in register

--- mandelbrot.s
+++ mandelbrot.s
@@ -1,14 +1,18 @@
 ; vim: ft=nasm
 
-%define MAX_ITERS 128
+%define MAX_ITERS 255
 
 ; graphics mode 13h
 %define WIDTH  320
 %define HEIGHT 200
+%define FWIDTH  320.
+%define FHEIGHT 200.
 
 ; register variables
-%define z xmm6
-%define c xmm7
+%define c xmm6 ; complex coordinate
+%define z xmm7 ; iterated variable
+%define px edi ; screen pixel x
+%define py esi ; screen pixel y
 
 bits 16
 org 0x7c00
@@ -25,14 +29,6 @@ org 0x7c00
 	or ax, 1<<9
 	mov cr4, eax
 
-	; init position multipliers
-	mov eax,  WIDTH/3
-	mov ebx, HEIGHT/2
-	cvtsi2ss xmm0, eax
-	cvtsi2ss xmm1, ebx
-	movss [mults.lo], xmm0
-	movss [mults.hi], xmm1
-
 init_palette:
 	mov dx, 3c8h ; palette index port
 	mov al, 0
@@ -46,20 +42,26 @@ init_palette:
 	mov al, cl
 	out dx, al
 	out dx, al
-	or  al, bl
+	add al, bl
 	out dx, al
 	inc bl
 	loop .color
 
+	; scaling factor
+	movlps xmm0, [scale]
+	divps  xmm0, [dim]
+	movlps [scale], xmm0
 draw:
 	; loop over pixels
-	mov dword [x], WIDTH-1
+	mov px, WIDTH
 .cols:
-	mov dword [y], HEIGHT-1
+	mov py, HEIGHT
 .rows:
 	; convert pixel position to complex coords
-	cvtpi2ps c, [pos]
-	divps    c, [mults]
+	cvtsi2ss c, py
+	unpcklps c, c
+	cvtsi2ss c, px
+	mulps    c, [scale]
 	subps    c, [offs]
 
 	; z := c
@@ -67,53 +69,40 @@ draw:
 	mov cl, MAX_ITERS
 .iter:
 	; z := z*z + c
-	xorps   xmm0, xmm0
-	movlhps z,    xmm0
-	movaps  xmm1, z
+	movsd   xmm0, z
 	shufps  z,    z,    14h ; z    =  a, b
-	shufps  xmm1, xmm1, 70h ; xmm1 =  a, a
-	movhlps xmm2, z         ; xmm2 =  b, a
-	movhlps xmm3, xmm1      ; xmm3 = -b, b
-	subss   xmm3, xmm2
+	shufps  xmm0, xmm0, 60h ; xmm0 =  a, a
+	movhlps xmm1, z         ; xmm1 =  b, a
+	movhlps xmm2, xmm0      ; xmm2 = -b, b
+	subss   xmm2, xmm1
 
-	mulps   z,    xmm1      ; z    =  aa, ba
-	mulps   xmm2, xmm3      ; xmm2 = -bb, ab
-
-	addps   z,    xmm2      ; z = aa-bb, ab+ab
+	mulps   z,    xmm0      ; z    =  aa, ba
+	mulps   xmm1, xmm2      ; xmm1 = -bb, ab
+	addps   z,    xmm1      ; z = aa-bb, ab+ab
 	addps   z,    c
-	cvtss2si eax, z         ; break if abs(z) exceeds 5
+	cvtss2si eax, z         ; break if abs(z) exceeds 4
 	cmp eax, 4
-	jge .pixel
+	jge .putpixel
 	loop .iter
 .done:
-	dec dword [y]
+	dec py
 	jge .rows
-	dec dword [x]
+	dec px
 	jge .cols
 	hlt
 
-.pixel:
+.putpixel:
 	mov eax, WIDTH
-	mov ebx, [y]
-	mul ebx
-	add eax, [x]
+	mul py
+	add eax, px
 	mov [es:eax], cl
 	jmp .done
 	ret
 
 align 8
-mults:
-.lo dd 0
-.hi dd 0
-
-offs:
-	dd 2.
-	dd 1.
-
-; screen position
-pos:
-x dd 0
-y dd 0
+dim   dd FWIDTH, FHEIGHT
+offs  dd 2., 1.
+scale dd 3., 2., 0, 0
 
 ; padding and bootsector magic number
 times 510-($-$$) db 0
